b0ab129e3fb30759e58d723a586b608e768df74d
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-03 20:07:29 +0300

    mv .container to global.css from Container.module.css idk if this is a good practice BUT i wold be easier to combine classes like this

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
 
-import css from './Container.module.css'
-
 interface ContainerProps {
   children: ReactNode;
 }
 
 export default function Container({ children }: ContainerProps) {
-  return <div className={css.container}>{children}</div>;
+  return <div className="container">{children}</div>;
 }
