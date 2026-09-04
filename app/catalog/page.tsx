64f4fb6672ddbcf1eeecf8c2ebe8831db14a680f
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-05 01:34:02 +0300

    upd Catalog page - useInfiniteQuery

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,12 +1,33 @@
+"use client";
+
 import CamperCard from "@/components/CamperCard/CamperCard";
 import Container from "@/components/Container/Container";
 import FiltersForm from "@/components/FiltersForm/FiltersForm";
 import { getCampers } from "@/lib/api";
 import css from "./page.module.css";
 import clsx from "clsx";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export default async function Catalog() {
-  const campers = await getCampers({});
+export default function Catalog() {
+  const { data, fetchNextPage } = useInfiniteQuery({
+    queryKey: ["campers"],
+    queryFn: ({ pageParam }) => {
+      return getCampers({ page: pageParam });
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastResponse) => {
+      const nextPage = lastResponse.page + 1;
+      return nextPage < lastResponse.totalPages ? nextPage : undefined;
+    },
+    enabled: true,
+    select: (data) => {
+      return {
+        ...data,
+        campers: data.pages.flatMap((page) => page.campers),
+      };
+    },
+  });
+
   return (
     <Container>
       <section className={css.section}>
@@ -15,13 +36,14 @@ export default async function Catalog() {
         </aside>
         <div className={css.campersListWrapper}>
           <ul className={css.campersList}>
-            {campers.campers.map((camper) => (
+            {data?.campers.map((camper) => (
               <li key={camper.id}>
                 <CamperCard camper={camper} />
               </li>
             ))}
           </ul>
           <button
+            onClick={() => fetchNextPage()}
             className={clsx(css.loadMoreButton, "buttonClear")}
             type="button"
           >
