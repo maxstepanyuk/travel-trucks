f7757e8014430432ef5d3a19af1ffe824e041a1a
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-04 23:17:48 +0300

    add lib/api.ts - getCampers(), interface GetCampersParams GetCampersResponse

---
+++ lib/api.ts
@@ -0,0 +1,49 @@
+import {
+  CamperEngine,
+  CamperForm,
+  CamperListItemDto,
+  CamperTransmission,
+} from "@/types/camper";
+import axios from "axios";
+
+const campersApi = axios.create({
+  baseURL: "https://campers-api.goit.study",
+});
+
+export interface GetCampersParams {
+  page?: number;
+  perPage?: number;
+  location?: string;
+  form?: CamperForm;
+  transmission?: CamperTransmission;
+  engine?: CamperEngine;
+}
+
+export interface GetCampersResponse {
+  page: number;
+  perPage: number;
+  total: number;
+  totalPages: number;
+  campers: CamperListItemDto[];
+}
+
+export async function getCampers({
+  page = 1,
+  perPage = 5,
+  location,
+  form,
+  transmission,
+  engine,
+}: GetCampersParams): Promise<GetCampersResponse> {
+  const res = await campersApi.get<GetCampersResponse>("/campers", {
+    params: {
+      page,
+      perPage,
+      location,
+      form,
+      transmission,
+      engine,
+    },
+  });
+  return res.data;
+}
