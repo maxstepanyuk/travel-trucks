ca71d9fe33353b1d1871e0857850791cfb80c585
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-04 23:23:00 +0300

    upd Catalog - add css - positions, Load More button

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,26 +1,39 @@
 import CamperCard from "@/components/CamperCard/CamperCard";
 import Container from "@/components/Container/Container";
 import FiltersForm from "@/components/FiltersForm/FiltersForm";
+import css from "./page.module.css";
+import clsx from "clsx";
 
 export default function Catalog() {
   return (
     <Container>
-      <h1>Catalog</h1>
-      <FiltersForm />
-      <ul style={{ display: "flex", gap: "32px", flexDirection: "column" }}>
-        <li>
-          <CamperCard />
-        </li>
-        <li>
-          <CamperCard />
-        </li>
-        <li>
-          <CamperCard />
-        </li>
-        <li>
-          <CamperCard />
-        </li>
-      </ul>
+      <section className={css.section}>
+        <aside>
+          <FiltersForm />
+        </aside>
+        <div className={css.campersListWrapper}>
+          <ul className={css.campersList}>
+            <li>
+              <CamperCard />
+            </li>
+            <li>
+              <CamperCard />
+            </li>
+            <li>
+              <CamperCard />
+            </li>
+            <li>
+              <CamperCard />
+            </li>
+          </ul>
+          <button
+            className={clsx(css.loadMoreButton, "buttonClear")}
+            type="button"
+          >
+            Load More
+          </button>
+        </div>
+      </section>
     </Container>
   );
 }
