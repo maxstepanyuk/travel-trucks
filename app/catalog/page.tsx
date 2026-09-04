c0721e6fc62961d77542c21dc7d27a4e98649745
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-04 23:42:44 +0300

    upd Catalog page - get and render campers from api

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,10 +1,12 @@
 import CamperCard from "@/components/CamperCard/CamperCard";
 import Container from "@/components/Container/Container";
 import FiltersForm from "@/components/FiltersForm/FiltersForm";
+import { getCampers } from "@/lib/api";
 import css from "./page.module.css";
 import clsx from "clsx";
 
-export default function Catalog() {
+export default async function Catalog() {
+  const campers = await getCampers({});
   return (
     <Container>
       <section className={css.section}>
@@ -13,18 +15,11 @@ export default function Catalog() {
         </aside>
         <div className={css.campersListWrapper}>
           <ul className={css.campersList}>
-            <li>
-              <CamperCard />
-            </li>
-            <li>
-              <CamperCard />
-            </li>
-            <li>
-              <CamperCard />
-            </li>
-            <li>
-              <CamperCard />
-            </li>
+            {campers.campers.map((camper) => (
+              <li key={camper.id}>
+                <CamperCard camper={camper} />
+              </li>
+            ))}
           </ul>
           <button
             className={clsx(css.loadMoreButton, "buttonClear")}
