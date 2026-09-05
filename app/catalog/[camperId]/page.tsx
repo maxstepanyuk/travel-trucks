40831419ffda4b2f28c4172fece06af602341858
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-05 22:55:50 +0300

    upd app\catalog\[camperId]\page.tsx Details - fetch and show json

--- app/catalog/[camperId]/page.tsx
+++ app/catalog/[camperId]/page.tsx
@@ -1,3 +1,24 @@
-export default function Catalog() {
-  return <h1>Camper id</h1>;
+"use client";
+
+import Container from "@/components/Container/Container";
+import { getCamperById } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+export default function Details() {
+  const { camperId } = useParams<{ camperId: string }>();
+
+  const { data: camper } = useQuery({
+    queryKey: ["camper", camperId],
+    queryFn: () => {
+      return getCamperById(camperId);
+    },
+  });
+
+  return (
+    <Container>
+      <h1>{camperId}</h1>
+      <pre>{JSON.stringify(camper, null, "  ")}</pre>
+    </Container>
+  );
 }
