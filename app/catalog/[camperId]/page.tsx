e7432e148a45a9219559ad14020c4016eb3a93b8
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-06 17:26:25 +0300

    upd Details page - use CamperDetailsGallerySwiper and add temp loader and not-found

--- app/catalog/[camperId]/page.tsx
+++ app/catalog/[camperId]/page.tsx
@@ -4,21 +4,31 @@ import Container from "@/components/Container/Container";
 import { getCamperById } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
+import css from "./page.module.css";
+import CamperDetailsGallerySwiper from "@/components/CamperDetailsGallerySwiper/CamperDetailsGallerySwiper";
+import LoaderSpinner from "@/components/LoaderSpinner/LoaderSpinner";
 
 export default function Details() {
   const { camperId } = useParams<{ camperId: string }>();
 
-  const { data: camper } = useQuery({
+  const { data: camper, isFetching } = useQuery({
     queryKey: ["camper", camperId],
     queryFn: () => {
       return getCamperById(camperId);
     },
   });
 
+  // todo center LoaderSpinner 
+  if (isFetching) return <LoaderSpinner />;
+  if (!camper) return <p>todo: not found</p>;
+
   return (
     <Container>
-      <h1>{camperId}</h1>
-      <pre>{JSON.stringify(camper, null, "  ")}</pre>
+      <div className={css.topSection}>
+        <CamperDetailsGallerySwiper gallery={camper?.gallery} />
+        {/* todo add info container here */}
+      </div>
+      <div className={css.reviewsSection}></div>
     </Container>
   );
 }
