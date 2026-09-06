e90640c08879cdb561aa87a6db4762d35c465e6f
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-06 20:16:46 +0300

    upd util.ts - add formatApiMeters formatApiLiters formatApiKwh formatApiConsumption

--- lib/util.ts
+++ lib/util.ts
@@ -17,3 +17,42 @@ export function abbreviationToUpperCase(text: string): string {
   }
   return text;
 }
+
+export function formatApiMeters(text: string): string {
+  if (text.endsWith("m")) {
+    return text.replace("m", " m");
+  }
+  return text;
+}
+
+export function formatApiLiters(text: string): string {
+  if (text.endsWith("l")) {
+    return text.replace("l", " L");
+  }
+  return text;
+}
+
+export function formatApiKwh(text: string): string {
+  if (text.endsWith("kWh")) {
+    return text.replace("kWh", " kWh");
+  }
+  return text;
+}
+
+export function formatApiConsumption(text: string): string {
+  let formatText = text;
+
+  if (formatText.includes("l")) {
+    formatText = formatText.replace("l", " l");
+  }
+
+  if (formatText.includes("/")) {
+    formatText = formatText.replace("/", " / ");
+  }
+
+  if (formatText.includes("km")) {
+    formatText = formatText.replace("km", " km");
+  }
+
+  return formatText;
+}
