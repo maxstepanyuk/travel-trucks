4215ac2d2b35d190f11d29a89dbdea728c7dbfb3
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-06 20:25:05 +0300

    upd Details page - use CamperDetailsInfoCard and ups css (and add todos)

--- app/catalog/[camperId]/page.tsx
+++ app/catalog/[camperId]/page.tsx
@@ -7,6 +7,7 @@ import { useParams } from "next/navigation";
 import css from "./page.module.css";
 import CamperDetailsGallerySwiper from "@/components/CamperDetailsGallerySwiper/CamperDetailsGallerySwiper";
 import LoaderSpinner from "@/components/LoaderSpinner/LoaderSpinner";
+import CamperDetailsInfoCard from "@/components/CamperDetailsInfoCard/CamperDetailsInfoCard";
 
 export default function Details() {
   const { camperId } = useParams<{ camperId: string }>();
@@ -18,17 +19,22 @@ export default function Details() {
     },
   });
 
-  // todo center LoaderSpinner 
+  // todo center LoaderSpinner and error. or error as toast
   if (isFetching) return <LoaderSpinner />;
   if (!camper) return <p>todo: not found</p>;
 
   return (
     <Container>
-      <div className={css.topSection}>
-        <CamperDetailsGallerySwiper gallery={camper?.gallery} />
-        {/* todo add info container here */}
-      </div>
-      <div className={css.reviewsSection}></div>
+      <section className={css.section}>
+        <div className={css.topSection}>
+          <CamperDetailsGallerySwiper gallery={camper?.gallery} />
+          <CamperDetailsInfoCard camper={camper} />
+        </div>
+      </section>
+      {/* <section className={css.section}>
+        <div className={css.reviewsSection}></div>
+        <div className={css.bookForm}></div>
+      </section> */}
     </Container>
   );
 }
