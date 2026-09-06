57f7facebe02a70a02561919eb562b3a60996e02
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-06 22:56:12 +0300

    upd CamperDetailsBookingForm - add validationSchema, upd css height

--- components/CamperDetailsBookingForm/CamperDetailsBookingForm.tsx
+++ components/CamperDetailsBookingForm/CamperDetailsBookingForm.tsx
@@ -1,8 +1,22 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import css from "./CamperDetailsBookingForm.module.css";
 import clsx from "clsx";
+import { BookingRequestDto } from "@/types/booking";
+import * as Yup from "yup";
+
+const initialValues: BookingRequestDto = {
+  name: "",
+  email: "",
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().min(2).required("Please enter your name."),
+  email: Yup.string().email().required("Please enter your name."),
+});
 
 export default function CamperDetailsBookingForm() {
+  const isPending = true;
+
   return (
     <div className={css.card}>
       <div className={css.title}>
@@ -11,41 +25,59 @@ export default function CamperDetailsBookingForm() {
           Stay connected! We are always ready to help you.
         </p>
       </div>
-      <Formik onSubmit={() => {}} initialValues={{}}>
-        <Form className={css.form}>
-          <div className={css.fields}>
-            <div className={css.fieldAndError}>
-              <Field
-                id="name"
-                type="text"
-                name="name"
-                className={css.input}
-                placeholder="Name*"
-              />
-              <ErrorMessage
-                component="span"
-                name="name"
-                className={css.error}
-              />
-            </div>
+      <Formik
+        onSubmit={() => {}}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, dirty, errors, touched }) => {
+          return (
+            <Form className={css.form}>
+              <div className={css.fields}>
+                <div className={css.fieldAndError}>
+                  <Field
+                    id="name"
+                    type="text"
+                    name="name"
+                    className={clsx(css.input, {
+                      [css.inputError]: touched.name && errors.name,
+                    })}
+                    placeholder="Name*"
+                  />
+                  <ErrorMessage
+                    component="span"
+                    name="name"
+                    className={css.error}
+                  />
+                </div>
 
-            <div className={css.fieldAndError}>
-              <Field
-                id="email"
-                type="email"
-                name="email"
-                className={css.input}
-                placeholder="Email*"
-              />
-              <ErrorMessage
-                component="span"
-                name="email"
-                className={css.error}
-              />
-            </div>
-          </div>
-          <button type="submit" className={clsx("buttonSolid", css.submit)}>Send</button>
-        </Form>
+                <div className={css.fieldAndError}>
+                  <Field
+                    id="email"
+                    type="email"
+                    name="email"
+                    className={clsx(css.input, {
+                      [css.inputError]: touched.email && errors.email,
+                    })}
+                    placeholder="Email*"
+                  />
+                  <ErrorMessage
+                    component="span"
+                    name="email"
+                    className={css.error}
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className={clsx("buttonSolid", css.submit)}
+                disabled={!isValid || !dirty || isPending}
+              >
+                {isPending ? "Creating..." : "Send"}
+              </button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
