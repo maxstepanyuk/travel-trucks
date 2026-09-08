d973a39bac5f784a366609872c09ed3c3f0984eb
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-09-08 12:11:42 +0300

    upd Catalog - add generateMetadata (dynamic and over-optimized)

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -6,8 +6,42 @@ import {
 } from "@tanstack/react-query";
 import { initialCatalogFilters } from "@/lib/store/filtersStore";
 import CatalogClient from "./Catalog.client";
+import { Metadata } from "next";
+import { formatLabelText, toFirstUpperLetter } from "@/lib/util";
 
-// todo meta
+export async function generateMetadata(): Promise<Metadata> {
+  const camperFilters = await getCamperFilters();
+
+  //todo? export to separate function (util.ts)
+  const filterText = Object.entries(camperFilters)
+    .map(([key, values]) => {
+      return (
+        toFirstUpperLetter(key) +
+        " (" +
+        values
+          .map((variant: string) => {
+            return formatLabelText(variant);
+          })
+          .join(", ") +
+        ")"
+      );
+    })
+    .join(", ")
+    .concat(".");
+
+  const metadata: Metadata = {
+    title: "Catalog - TravelTrucks",
+    description:
+      "Catalog of campers. With ability to filter by City, " + filterText,
+    openGraph: {
+      title: "Catalog - TravelTrucks",
+      description:
+        "Catalog of campers. With ability to filter by City, " + filterText,
+    },
+  };
+
+  return metadata;
+}
 
 const INIT_PAGE_NUMBER = 1;
 
